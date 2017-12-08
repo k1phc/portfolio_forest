66e8d077dc86c6e68d98d0ca53a0edb29bfd7605
Kirill Sinyushkin <abigor_89@mail.ru>
2017-12-08 23:17:43 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,28 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
-    output: {
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new UglifyJSPlugin({
-            sourceMap: true
-        })
-    ]
+  output: {
+    filename: 'bundle.js'
+ },
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true,
+    }),
+  ],
+  module: 
+    rules: [
+            
+      {
+            test: /\.js$/,
+            enforce: "pre",
+            loader: "eslint-loader",
+            options: {
+                    fix: true
+                }            
+            },
+                                     
+        ]
+    }
 };
 
 module.exports = config;
